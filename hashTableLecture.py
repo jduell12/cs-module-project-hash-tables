e8a9c29ee23e6f037662e3f27cc0f71823784af7
Jessica <duellje@oregonstate.edu>
2020-09-28 13:09:25 -0400

    added hash entry class

--- hashTableLecture.py
+++ hashTableLecture.py
@@ -1,39 +1,48 @@
 #list to hold hashing stringbased on the has number
+#length of list is a power of 2 
 table = [None] * 8
+
+class HashEntry:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        
+    def __repr__(self):
+        return f"HashEntry({self.key}, {self.value}"
 
 #Naive hashing function - shouldn't be used
 def hashf(s):
     total = 0;
-    
     #encode string into bytes to be able to use unicode characters
     bytes = str(s).encode()
-    
-    for b in bytes: #O(n) over length of the key/string s
+    #O(n) over length of the key/string s
+    for b in bytes: 
         #add the byte to total
         total += b
-    
     return total 
 
 def get_index(s):
     value = hashf(s);
     return value % len(table)
 
-def put(key, value):
+def put(key, value): 
+    #O(n) over the length of the key
+    #O(1) over the number of items in the table 
     index = get_index(key)
-    table[index] = value
+    table[index] = HashEntry(key, value)
 
 def get(key):
     index = get_index(key)
-    value = table[index]
-    if value:
-        return value
+    hash_entry = table[index]
+    if hash_entry:
+        return hash_entry
     else:
         return f'{key} is not in the table'
 
 put("Beej", 3490)
-print(table)
+# print(table)
 put("Goats!", 9999)
-print(table)
+# print(table)
 
 print(get("Beej"))
 print(get("Goats!"))
