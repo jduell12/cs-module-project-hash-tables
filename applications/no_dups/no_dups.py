1bd3f1fc545a5167bc7cd71afcf2dee9a69325ad
Jessica <duellje@oregonstate.edu>
2020-09-30 16:23:39 -0400

    getting no dups to work

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,11 +1,45 @@
 def no_dups(s):
-    # Your code here
+    
+    if len(s) < 1:
+        return s
+    
+    check = {}
+    
+    #split string via whitespace
+    words = s.split()
+    
+    #add the word with their index into the dict
+    for index in range(len(words)):
+        check[index] = words[index]
+        
+    #getting array of keys with values
+    checkList = list(check.items())
+    
+    #sort by values
+    checkList.sort(key=lambda t:t[1])
+    print("------------------")
+    print(checkList)
+    print("------------------")
+    
+    for checkIndex in range(len(check)):
+        if checkIndex+1 < len(check) and checkList[checkIndex][0] == checkList[checkIndex+1][0]:
+            checkList[checkIndex+1] = (checkIndex+1, '')
+    
+    #sort list by index to put words back in order
+    checkList.sort()
+    
+    return checkList
+        
+    # word = " "
+    
+    # return word.join(words).strip()
+    
 
 
 
 if __name__ == "__main__":
-    print(no_dups(""))
-    print(no_dups("hello"))
-    print(no_dups("hello hello"))
-    print(no_dups("cats dogs fish cats dogs"))
+    # print(no_dups(""))
+    # print(no_dups("hello"))
+    # print(no_dups("hello hello"))
+    # print(no_dups("cats dogs fish cats dogs"))
     print(no_dups("spam spam spam eggs spam sausage spam spam and spam"))
