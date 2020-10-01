c72ba6543c57125c66cfaf7c396ebbc73e15e3a4
Jessica <duellje@oregonstate.edu>
2020-10-01 18:20:25 -0400

    completed caeser

--- applications/crack_caesar/crack_caesar.py
+++ applications/crack_caesar/crack_caesar.py
@@ -1,5 +1,12 @@
 #frequent letters from most to least 
-eng_freq = ['E', 'T', 'A', 'O', 'H', 'N', 'R', 'I', 'S', 'D', 'L', 'W', 'U', 'G', 'F', 'B', 'M', 'Y', 'C', 'P', 'K', 'V', 'Q', 'J', 'X', 'Z']
+eng_freq = {'E': '11.53', 'T':'9.75', 'A':'8.46', 'O':'8.08', 'H':'7.71', 'N':'6.73', 'R':'6.29', 'I':'5.84', 'S':'5.56', 'D':'4.74', 'L':'3.92', 'W':'3.08', 'U':'2.59', 'G':'2.48', 'F':'2.42', 'B':'2.19', 'M':'2.18', 'Y':'2.02', 'C':'1.58', 'P':'1.08', 'K':'0.84', 'V':'0.59', 'Q':'0.17', 'J':'0.07', 'X':'0.07', 'Z':'0.03'}
+
+invert_eng_freq = {}
+
+for k in eng_freq:
+    v = float(eng_freq[k])
+    invert_eng_freq[v] = k
+
 
 def crack_caesar(fileName):
     cipher_freq = {}
@@ -23,10 +30,23 @@ def crack_caesar(fileName):
         length += 1
         
     for k in cipher_freq:
-        cipher_freq[k] = cipher_freq[k] / length
+        cipher_freq[k] = f"{(cipher_freq[k] / length) * 100:.2f}"
         
-    print(cipher_freq)
-        
+    #key -> cipher text, value -> letter it should be
+    cipher_decoder = {}
     
+    for item in cipher_freq:
+        cipher_decoder[item] = invert_eng_freq[float(cipher_freq[item])]
+        
+    #decodes the message 
+    decoded = ""
+    
+    for c in words:
+        if c in cipher_decoder:
+            decoded += cipher_decoder[c]
+        else:
+            decoded += c
+            
+    print(decoded)
     
 crack_caesar('ciphertext.txt')
