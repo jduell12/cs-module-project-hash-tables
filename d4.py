66685b6b8c9fcf944dc3cb2d6f51a6b3d9b76288
Jessica <duellje@oregonstate.edu>
2020-10-01 12:24:45 -0400

    indexing

--- d4.py
+++ d4.py
@@ -12,6 +12,42 @@ def get_inv_sqrt(n):
         inv_sqrt[n] = 1/math.sqrt(n)
     return inv_sqrt[n]
 
-build_lookup_table()
-print(get_inv_sqrt(37))
-print(get_inv_sqrt(2000))
+# build_lookup_table()
+# print(get_inv_sqrt(37))
+# print(get_inv_sqrt(2000))
+
+#########################################################
+#indexing
+#names, department
+records = [
+    ("Alice", "Engineering"),
+    ("Bob", "Sales"),
+    ("Carol", "Sales"),
+    ("Dave", "Engineering"),
+    ("Erin", "Engineering"),
+    ("Frank", "Engineering"),
+    ("Grace", "Marketing")
+]
+
+#Who are all the people in a given department?
+
+#naive
+# d = "Engineering"
+
+# for r in records: #O(n) over number of records 
+#     if r[1] == d:
+#         print(r[0])
+
+#O(1) solution - hash tables
+#key --> department
+#value --> list of names
+
+department_index = {}
+
+for r in records:
+    if r[1] in department_index:
+        department_index[r[1]].append(r[0])
+    else:
+        department_index[r[1]] = [r[0]]
+
+print(department_index["Marketing"])
