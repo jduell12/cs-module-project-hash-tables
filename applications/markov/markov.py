7d3eed4d92769de20fe42b9790f0088f006a8e8e
Jessica <duellje@oregonstate.edu>
2020-10-01 12:14:27 -0400

    added lecture notes

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -2,10 +2,26 @@ import random
 
 # Read in all the words in one go
 with open("input.txt") as f:
-    words = f.read()
+    #splits the input into words
+    words = f.read().split()
 
-# TODO: analyze which words can follow other words
-# Your code here
+#gets all the starting words
+#Words that start with capital or "
+start_words = {}
+
+#gets all ending words 
+#words end in any type of punctuation or followed by "
+end_words = {}
+
+for i in range(len(words)):
+    word = words[i]
+    c = word[0]
+    if ord(c) == 34 or (ord(c) >= 65 and ord(c) <= 90):
+        if (word, words[i+1]) in start_words:
+            start_words[(word, words[i+1])] += 1
+        else:
+            start_words[(word, words[i+1])] = 1
+            
 
 
 # TODO: construct 5 random sentences
