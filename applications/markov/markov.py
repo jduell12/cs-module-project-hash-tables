786da224978a667d44a8eed00c6632a2758c0024
Jessica <duellje@oregonstate.edu>
2020-10-01 14:18:01 -0400

    getting garbled sentences - what we want

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -7,23 +7,59 @@ with open("input.txt") as f:
 
 #gets all the starting words
 #Words that start with capital or "
-start_words = {}
+start_words = []
 
 #gets all ending words 
 #words end in any type of punctuation or followed by "
-end_words = {}
+end_words = []
 
-for i in range(len(words)):
+word_freq = {}
+
+for i in range(len(words)-1):
     word = words[i]
+    #gets first letter
     c = word[0]
     if ord(c) == 34 or (ord(c) >= 65 and ord(c) <= 90):
-        if (word, words[i+1]) in start_words:
-            start_words[(word, words[i+1])] += 1
-        else:
-            start_words[(word, words[i+1])] = 1
+        if word not in start_words:
+            start_words.append(word)
             
+    else:
+        #gets end words
+        end = word[len(word)-1]
+        
+        if ord(end) == 33 or ord(end) == 63 or ord(end) == 46 or ord(end) == 34:
+            if word not in end_words:
+                end_words.append(word)
+        else:
+            if words[i+1] != len(words)-1:
+                if (word, words[i+1]) in word_freq:
+                    word_freq[(word, words[i+1])] += 1
+                else:
+                    word_freq[(word, words[i+1])] = 1
+            
+#create list of dictionary
+list = list(word_freq.items())
 
-
-# TODO: construct 5 random sentences
-# Your code here
-
+#construct 5 random sentences
+for sent in range(5):
+    s = ""
+    #get random first word
+    s += random.choice(start_words)
+    
+    #get number of words for the sentence 
+    #on average english sentenances contain 15-20 words per wordcounter.net
+    numWords = random.randint(14, 21)
+    
+    while numWords > 0:
+        #gets random number
+        randWord = random.randint(0, len(word_freq))
+        word1 = list[randWord][0][0]
+        word2 = list[randWord][0][1]
+        s += f" {word1} {word2} "
+        numWords -= 2
+    
+    #adds end word on sentenance
+    s += random.choice(end_words)
+    
+    print(s)
+    print("")
