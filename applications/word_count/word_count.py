1f2aa17cd71f830aa9c26bf0c33fdfe8bd942f5d
Jessica Duell <duellrdh@gmail.com>
2020-11-11 21:50:46 -0500

    Better solution.
    Found a better solution to the problem.

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,28 +1,43 @@
 def word_count(s):
+    # Your code here
     count = {}
-    #ignore " : ; , . - + = / \ | [ ] { } ( ) * ^ & via Ascii
-    ignore = [34, 58, 59, 44, 46, 45, 43, 61, 47, 92, 124, 91, 93, 123, 125,40, 41, 42, 94, 38]
-    
-    #check if string has a length
-    if len(s) > 0:
-        #split string using whitespace
-        words = s.split()
-        #go through each word in the string 
-        for word in words:
-            #change word to lower case 
-            word = word.lower()
-            #go through each character in the word
-            for c in word:
-                if ord(c) in ignore:
-                    word = word.translate({ord(c):None})
-                    
-            if word not in count:
-                if len(word) > 0:
-                    count[word] = 1
-            else: 
-                count[word] += 1
-    
+    s_list = s.split()
+    for word in s_list:
+        word = word.strip('":;,.-+=/\\|[]}{()*^&')
+        if word == '':
+            continue
+        word = word.lower()
+        if word not in count:
+            count[word] = 1
+        else:
+            count[word] += 1
     return count
+
+# def word_count(s):
+#     count = {}
+#     #ignore " : ; , . - + = / \ | [ ] { } ( ) * ^ & via Ascii
+#     ignore = [34, 58, 59, 44, 46, 45, 43, 61, 47, 92, 124, 91, 93, 123, 125,40, 41, 42, 94, 38]
+    
+#     #check if string has a length
+#     if len(s) > 0:
+#         #split string using whitespace
+#         words = s.split()
+#         #go through each word in the string 
+#         for word in words:
+#             #change word to lower case 
+#             word = word.lower()
+#             #go through each character in the word
+#             for c in word:
+#                 if ord(c) in ignore:
+#                     word = word.translate({ord(c):None})
+                    
+#             if word not in count:
+#                 if len(word) > 0:
+#                     count[word] = 1
+#             else: 
+#                 count[word] += 1
+    
+#     return count
 
 
 
